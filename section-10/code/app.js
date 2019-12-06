50657e9147f85428c090401867cb6e32a8b3ea1d
ramvishvas <ramvishvasg@gmail.com>
2019-12-06 06:03:04 +0530

    6 dec 2019

--- section-10/code/app.js
+++ section-10/code/app.js
@@ -1,9 +1,17 @@
-const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const errorController = require('./controllers/error');
+const db = require('./util/database');
+
+db.execute('SELECT * FROM products')
+    .then(res => {
+        console.log('Results: ', res);
+    })
+    .catch(err => {
+        console.error(err);
+    });
 
 const app = express();
 
@@ -13,7 +21,9 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
